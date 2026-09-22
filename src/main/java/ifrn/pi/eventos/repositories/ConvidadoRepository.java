66c5bf5c0dd177fe3289c2495199bcd2453925c6
Luana <letycia.l@escolar.ifrn.edu.br>
2026-09-22 18:19:36 -0300

    Implementando as funcionalidades de editar evento e convidado

--- src/main/java/ifrn/pi/eventos/repositories/ConvidadoRepository.java
+++ src/main/java/ifrn/pi/eventos/repositories/ConvidadoRepository.java
@@ -10,6 +10,6 @@ import ifrn.pi.eventos.models.Evento;
 public interface ConvidadoRepository extends JpaRepository<Convidado, Long> {
 	
 	List<Convidado> findByEvento(Evento evento);
-	Convidado findByRg(String rg);
+	
 
 }
